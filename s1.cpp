3295e742c7084a79772aa8ae0b4339adfdb8d784
Girish <skillqode.gk@gmail.com>
2023-06-21 10:12:26 +0530

    for loop

---
+++ s1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,total=0;
+    for(i=0;i<=10;i++)
+    {
+        cout<< i;
+        total+=i;
+    }
+    cout<<"\ntotal is:"<<total;
+}
