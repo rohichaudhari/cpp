dc0c760d8c427c64ab705342b3bf9c1416f59a00
Girish <skillqode.gk@gmail.com>
2023-06-21 09:47:00 +0530

    main

---
+++ add.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter a:";
+    cin>>a;
+     cout<<"enter b:";
+    cin>>b;
+    int c=a+b;
+    cout<<"addition is:"<<c;
+
+}
