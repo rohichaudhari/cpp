decec240dc4140e23c36d7e2ac0853a4b8ea2225
Girish <skillqode.gk@gmail.com>
2023-07-01 09:09:24 +0530

    1/7/2023

--- cube.cpp
+++ cube.cpp
@@ -8,16 +8,25 @@ class SOLID
     {
         cout<<"volume of cube is:"<<a*a*a<<endl;
     }
-    SOLID(double l,int b,int h)
+    SOLID(int l,int b,int h)
     {
         cout<<"volume of cuboid:"<<l*b*h<<endl;
     }
-    SOLID(int r,int h)
+    SOLID(double r,double h)
     {
-        cout<<"volume of cone:"<<1/3*22/7*r*r*h<<endl;
+      cout<<"volume of cylinder is:"<<3.14*r*r*h<<endl;
     }
+    SOLID(double r)
+    {
+        cout<<"volume of sphere:"<<4*3.14*r*r*r/3<<endl;
+    }
+    SOLID(double r,int h)
+    {
+        cout<<"volume of cone:"<<3.14*r*r*h/3<<endl;
+    }
+    
 };
 int main()
 {
-    SOLID a(10),b(2.5,8,9),c(4,7);
+    SOLID a(10),b(5,8,9),c(4.4,7.6),d(2.2),e(4.4,6);
 }
