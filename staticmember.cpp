7f28eddb37398dc7e15196de39bb7a599a9110b6
Girish <skillqode.gk@gmail.com>
2023-07-14 09:55:33 +0530

    14/7/2023

---
+++ staticmember.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class test
+{
+    public:
+    static int a;
+    test()
+    {
+        cout<<"test calling.."<<endl;
+        ++a;
+    }
+    static int member()
+    {
+        return a;
+    }
+};
+int test::a=50;
+int main()
+{
+    cout<<"starting point:"<<test::member()<<endl;
+    test t1,t2;
+cout<<"ending point:"<<test::member()<<endl;
+}
